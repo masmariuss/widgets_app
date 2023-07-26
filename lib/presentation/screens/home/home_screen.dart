fd3e8494645f988471c42e9d5868a53e29090767
masmariuss <111192368+masmariuss@users.noreply.github.com>
2023-07-26 21:22:40 +0200

    cards finished

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:widgets_app/config/theme/menu/menu_items.dart';
+import 'package:widgets_app/presentation/screens/buttons/buttons_screen.dart';
 import 'package:widgets_app/presentation/screens/cards/cards_screen.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -65,8 +66,9 @@ class _CustomListTile extends StatelessWidget {
         //); 
         // Navigator.pushNamed(context, menuItem.link);
 
-        // context.push(menuItem.link);
-        context.pushNamed( CardsScreen.name );
+        context.push(menuItem.link);
+        // context.pushNamed( CardsScreen.name );
+        // context.pushNamed( ButtonsScreen.name );
       },
     );
   }
